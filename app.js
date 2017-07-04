3d42a9ceb8e843da73f19ee5a5d197d8595452a8
bazzika <darya.belob@gmail.com>
2017-07-04 12:23:58 +0300

    create js file

--- app.js
+++ app.js
@@ -1,3 +1,24 @@
 /**
  * Created by Даша on 04.07.2017.
  */
+const element = '# Heading\n=======\n\nSub-heading\n-----------\n' +
+  ' \n### Another deeper heading\n \nParagraphs are separated\n' +
+  'by a blank line.\n\nLeave 2 spaces at the end of a line to do a  \n' +
+  'line break\n\nText attributes *italic*, **bold**, \n' +
+  '`monospace`, ~~strikethrough~~ .\n\nShopping list:\n\n  * apples\n' +
+  '  * oranges\n  * pears\n\nNumbered list:\n\n  1. apples\n  2. oranges\n' +
+  '  3. pears\n\nThe rain---not the reign---in\nSpain.\n\n' +
+  ' *[Herman Fassett](https://freecodecamp.com/hermanfassett)*';
+var Markdown = React.createClass ({
+
+
+
+
+
+
+});
+ReactDOM.render(
+<Markdown />,
+
+
+);
