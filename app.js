6f3e084d44e7a90e09132d72dcc2170cd22599a4
bazzika <darya.belob@gmail.com>
2017-07-06 17:49:01 +0300

    fix changing of typing text

--- app.js
+++ app.js
@@ -1,7 +1,5 @@
-/**
- * Created by Даша on 04.07.2017.
- */
-const element = '# Heading\n=======\n\nSub-heading\n-----------\n' +
+
+const textCommon = '# Heading\n=======\n\nSub-heading\n-----------\n' +
   ' \n### Another deeper heading\n \nParagraphs are separated\n' +
   'by a blank line.\n\nLeave 2 spaces at the end of a line to do a  \n' +
   'line break\n\nText attributes *italic*, **bold**, \n' +
@@ -10,15 +8,34 @@ const element = '# Heading\n=======\n\nSub-heading\n-----------\n' +
   '  3. pears\n\nThe rain---not the reign---in\nSpain.\n\n' +
   ' *[Herman Fassett](https://freecodecamp.com/hermanfassett)*';
 var Markdown = React.createClass ({
+  getInitialState: function() {
+    return {
+      textCommon:textCommon
+    }
+  },
+  onChange: function(event){
+    var textarea = event.target;
+    this.setState({
+      textCommon:textarea.value
+    });
+  },
 
+  render: function() {
+    return (
+      < div className = 'row'>
+          <div className = 'left col-md-6'>
+            <textarea onChange = {this.onChange} type ='text' className='edit'>
+              {textCommon}
+              </textarea>
+          </div>
+        < div className = 'right col-md-6' dangerouslySetInnerHTML = {{__html:marked(this.state.textCommon)}}>
 
-
-
-
-
+          </div>
+      </div>
+    )
+  }
 });
 ReactDOM.render(
 <Markdown />,
-document.getElementsByClassName('container')
-
+document.querySelector('.container')
 );
